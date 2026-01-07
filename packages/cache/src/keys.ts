d45dd75367347a03d8710c6c37fde57328df9892
Danny Wieser <codeiscraft@gmail.com>
2026-01-07 07:47:10 -0700

    more tests

--- packages/cache/src/keys.ts
+++
@@ -1,6 +0,0 @@
-const keySeparator = ':'
-
-export const generateUserKey = (userId: string, prefix: string, key: string) =>
-  `${userId}${keySeparator}${prefix}${keySeparator}${key}`
-
-export const getLastKeySegment = (key: string) => key.split(keySeparator).pop() || ''
