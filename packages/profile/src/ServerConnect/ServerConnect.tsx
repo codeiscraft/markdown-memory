2998095c295f84ec303268edb148f2d03f831840
code is craft <codeiscraft@gmail.com>
2026-01-12 07:34:10 -0700

    display server details after connecting

--- packages/profile/src/ServerConnect/ServerConnect.tsx
+++ packages/profile/src/ServerConnect/ServerConnect.tsx
@@ -1,4 +1,14 @@
-import { Field, Group, IconButton, Input, Stack } from '@chakra-ui/react'
+import {
+  Em,
+  Field,
+  Group,
+  HoverCard,
+  IconButton,
+  Input,
+  Portal,
+  Stack,
+  Text,
+} from '@chakra-ui/react'
 import { useServerIdentity } from '@mdm/application'
 import { Check, CircleQuestionMark } from 'lucide-react'
 import { ChangeEvent, useState } from 'react'
@@ -12,13 +22,28 @@ export function ServerConnect() {
   const [serverRoot, setServerRoot] = useState('')
 
   const isValid = serverRoot.length === 0 || /^https?:\/\/\S+$/.test(serverRoot)
-  const { isFetching, isSuccess } = useServerIdentity(isValid ? serverRoot : undefined)
+  const {
+    data: identity,
+    isFetching,
+    isSuccess,
+  } = useServerIdentity(isValid ? serverRoot : undefined)
 
   const update = (event: ChangeEvent<HTMLInputElement>) => setServerRoot(event.target.value)
 
   const serverValid = isValid && isSuccess
   const icon = serverValid ? <Check /> : <CircleQuestionMark />
   const color = serverValid ? 'green.500' : 'gray.500'
+  const serverDetails =
+    serverValid && identity ? (
+      <Stack>
+        <Em>{serverRoot}</Em>
+        <Text>
+          {identity.product} v{identity.version}
+        </Text>
+      </Stack>
+    ) : (
+      'please enter a valid markdown memory server address'
+    )
 
   return (
     <Stack>
@@ -32,10 +57,21 @@ export function ServerConnect() {
             size="sm"
             value={serverRoot}
           />
-
-          <IconButton color={color} loading={isFetching} size="sm" variant="outline">
-            {icon}
-          </IconButton>
+          <HoverCard.Root size="sm">
+            <HoverCard.Trigger asChild>
+              <IconButton color={color} loading={isFetching} size="sm" variant="outline">
+                {icon}
+              </IconButton>
+            </HoverCard.Trigger>
+            <Portal>
+              <HoverCard.Positioner>
+                <HoverCard.Content>
+                  <HoverCard.Arrow />
+                  {serverDetails}
+                </HoverCard.Content>
+              </HoverCard.Positioner>
+            </Portal>
+          </HoverCard.Root>
         </Group>
       </Field.Root>
     </Stack>
