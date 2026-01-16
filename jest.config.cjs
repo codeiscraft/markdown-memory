a855566eb183186b0d09f0c0c4be46a993cc09f1
code is craft <codeiscraft@gmail.com>
2026-01-16 08:40:42 -0600

    rename

--- jest.config.cjs
+++ jest.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
     '<rootDir>/packages/components/jest.config.js',
     '<rootDir>/packages/notes/jest.config.js',
     '<rootDir>/packages/profile/jest.config.cjs',
-    '<rootDir>/packages/serverStatus/jest.config.cjs',
+    '<rootDir>/packages/server-status/jest.config.cjs',
     '<rootDir>/packages/testing-support/jest.base.config.cjs',
     '<rootDir>/packages/utils/jest.config.cjs',
     '<rootDir>/apps/server/jest.config.cjs',
